7fa619eb0d0c65a52f21501d5cbaf2e9a343ed9a
craftomeCJ <tech@craftome.com>
2025-02-08 16:16:38 +0800

    [MOBILE-CODE-TEST][CJ][DASHBOARD][Add]: dashboard constants and types
    - Introduces constants for dashboard tabs, grid items, status cards, and shows
    - Defines TypeScript interfaces for TabItem, GridItem, StatusCard, and Show

---
+++ src/constants/dashboard-constants.ts
@@ -0,0 +1,43 @@
+import { GridItem, Show, StatusCard, TabItem } from '../types/types';
+
+export const TAB_ITEMS: TabItem[] = [
+  { name: 'Map' },
+  { name: 'Inhabitants' },
+  { name: 'Shows' },
+  { name: 'Shopping' },
+  { name: 'Dine' },
+  { name: 'Meet & Greets' },
+];
+
+export const GRID_ITEMS: GridItem[] = [
+  { name: 'Map', icon: 'map-icon' },
+  { name: 'Inhabitants', icon: 'inhabitants-icon' },
+  { name: 'Shows', icon: 'shows-icon' },
+  { name: 'Shopping', icon: 'shopping-icon' },
+  { name: 'Dine', icon: 'dine-icon' },
+  { name: 'Meet & Greets', icon: 'meet-and-greets-icon' },
+];
+
+export const STATUS_CARDS: StatusCard[] = [
+  {
+    title: 'My e-tickets',
+    text: "There aren't any yet",
+    buttonText: 'Retrieve here',
+  },
+  {
+    title: 'Park hours',
+    text: 'Today, 13 Feb\n10am - 5pm',
+    buttonText: 'Plan my visit',
+  },
+];
+
+export const SHOWS: Show[] = [
+  { time: '2:30 PM', title: 'Dive Feeding @ Shipwreck' },
+  { time: '2:40 PM', title: 'Say Cheese Shark' },
+  {
+    time: '2:30 PM',
+    title:
+      'Dive Feeding @ Shipwreck Dive Feeding @ Shipwreck Dive Feeding @ Shipwreck Dive Feeding @ Shipwreck',
+  },
+  { time: '2:40 PM', title: 'Say Cheese Shark' },
+];
---
+++ src/types/types.ts
@@ -0,0 +1,19 @@
+export interface TabItem {
+  name: string;
+}
+
+export interface GridItem {
+  name: string;
+  icon: string;
+}
+
+export interface StatusCard {
+  title: string;
+  text: string;
+  buttonText: string;
+}
+
+export interface Show {
+  time: string;
+  title: string;
+}
